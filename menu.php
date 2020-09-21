65a600ea1508791ca665504a653020c84e42fa91
Isaac Murillo <imurillo@iu.edu>
2020-09-21 11:20:21 -0400

    testing

---
+++ menu.php
@@ -0,0 +1,20 @@
+	  
+
+
+
+<div id="menubar_container">
+		
+	<div id="menubar">
+		<div id="menu_items">
+	       		<ul id="menu">
+             			<li><a href="index.php">Home</a></li>
+              			<li><a href="judge.php">Judge</a></li>
+              			<li><a href="adminstrator.php">Adminstrator</a></li>
+              			<li><a href="projects.php">Projects</a></li>
+              			<li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+          	</div><!--close menu-->
+		
+        </div><!--close menubar-->
+	
+</div><!--close menubar_container-->
