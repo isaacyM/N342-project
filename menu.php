e449613bb38e937c12b200a212e612a86c717f46
riyshres <riyshres@iu.edu>
2020-10-12 17:17:19 -0400

    some changes in menu

--- menu.php
+++ menu.php
@@ -1,6 +1,5 @@
 		
 <!-- Menu -->
-<<<<<<< HEAD
 <nav id="menu">
 	<ul class="links">
 		<li><a href="index.php">Home</a></li>
@@ -11,16 +10,3 @@
 		<li><a href="#" class="button fit">Login</a></li>
 	</ul>
 </nav>
-=======
-	<nav id="menu">
-		<ul class="links">
-			<li><a href="index.php">Home</a></li>
-			<li><a href="generic.php">Generic</a></li>
-			<li><a href="elements.php">Elements</a></li>
-			<li><a href="register.php">Register</a></li>
-		</ul>
-		<ul class="actions vertical">
-			<li><a href="login.php" class="button fit">Login</a></li>
-		</ul>
-	</nav>
->>>>>>> c33e690289366f1b46c71ff7e7bfe60652c4488d
