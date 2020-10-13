29d1f5c9dc7f6c96e7db485a5defe9af0305387a
Isaac Murillo <imurillo@iu.edu>
2020-10-12 23:16:07 -0400

    footer

--- footer.php
+++ footer.php
@@ -1,21 +1,14 @@
-<!-- Footer -->
-<footer id="footer">
-    <div class="inner">
-        <h2>Get In Touch</h2>
-        <div class="row">
-            <div class="col-sm-4">
-                <span class="icon fa-map-marker"></span> 864 E Cambridge Dr.<br />Terre Haute, IN United States
-            </div>
-            <div class="col-sm-4">
-                <span class="icon fa-phone"></span> <a href="#">(812) 201-6931</a></li>
-            </div>
-            <div class="col-sm-4">
-                <span class="icon fa-envelope"></span> <a href="#">gcook@sefi.org</a></li>
-            </div>
-        </div>
-        
-    </div>
-    <div class="copyright">
-        &copy; Copyright 2015 Science Education Foundation of Indiana, Inc.| All Rights Reserved</a>.
-    </div>
-</footer>
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<h2>Get In Touch</h2>
+					<ul class="actions">
+						<li><span class="icon fa-phone"></span> <a href="#">(000) 000-0000</a></li>
+						<li><span class="icon fa-envelope"></span> <a href="#">information@untitled.tld</a></li>
+						<li><span class="icon fa-map-marker"></span> 123 Somewhere Road, Indianapolis, IN 00000</li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; Untitled. Design <a href="https://templated.co">TEMPLATED</a>. Images <a href="https://unsplash.com">Unsplash</a>.
+				</div>
+			</footer>
--- index.php
+++ index.php
@@ -29,23 +29,10 @@
 			</section>
 
 
-		<!-- Footer -->
-			<footer id="footer">
-				<div class="inner">
-					<h2>Get In Touch</h2>
-					<ul class="actions">
-						<li><span class="icon fa-phone"></span> <a href="#">(000) 000-0000</a></li>
-						<li><span class="icon fa-envelope"></span> <a href="#">information@untitled.tld</a></li>
-						<li><span class="icon fa-map-marker"></span> 123 Somewhere Road, Indianapolis, IN 00000</li>
-					</ul>
-				</div>
-				<div class="copyright">
-					&copy; Untitled. Design <a href="https://templated.co">TEMPLATED</a>. Images <a href="https://unsplash.com">Unsplash</a>.
-				</div>
-			</footer>
 
 		<!-- Scripts -->
 		<?php 
+			include "footer.php";
 			include "script.php";
 		?>
 	</body>
