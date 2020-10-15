24b334f8537168048663b4b75c3d0a3356128958
riyshres <riyshres@iu.edu>
2020-10-14 20:30:16 -0400

    changed collapsible to tabs

--- booth.php
+++ booth.php
@@ -1,5 +1,5 @@
 <?php
-	require_once("aSession.php");
+	require_once "aSession.php";
 	if(checkAdminSession())
 	{
 		header("Location: login.php");
@@ -23,125 +23,116 @@
 		<h1 align = "center"><a href="#">Booth Number</a></h1>
 		
 		<!-- Main -->
-		<section id="main" class="wrapper">
-			<div class="container"> 
-				<div class="12u$">
-					<button data-toggle="collapse" data-target="#enter"><i class="fas fa-sign-in-alt"> Enter new Booth Number</i></button>
-					<div id="enter" class="collapse">
-						<div class="inner"> 
+		<div class="container">	
+			<button class="tablink" onclick="openPage('show', this, '#ff9900')">Existing Booth</button>
+			<button class="tablink" onclick="openPage('enter', this, '#ff9900')" id="defaultOpen">Enter new Booth Number</button>
+			<!--First Tab Content-->
+            <div id="show" class="tabcontent">
+				<h1>Existing Content</h1>
+				<p>gggg</p>
+				<input type="reset" name ="reset" value="Reset" class="alt" />
+				<h2>ggg</h2>
+			</div><!--close #show tab-->
+
+            <!--Second Tab Content-->
+			<div id="enter" class="tabcontent">
+				<div class="inner"> 
 							
-							<!--PHP Code--->
+					<!--PHP Code--->
+					<?php
+						//always initialize variables to be used
+						$boothNumber = "";
+						$active = "";
+						$msg = "";
+
+						$yesChecked = "";
+						$noChecked = "";
+						$everythingOk= false;
+
+						if (isset($_POST['submit'])) //check if this page is requested after Submit button is clicked
+						{
+					
+							//take the information submitted and send to a process file
+							//always trim the user input to get rid of the additiona white spaces on both ends of the user input
+							$boothNumber = trim($_POST['boothNumber']);
+
+							//Active
+							if (isset($_POST['active']))
+								$active = trim($_POST['active']);
+							//taking the selected value for active
+							if ($active=="Yes") 
+							{
+								$yesChecked="checked";
+								$noChecked="";
+							}
+							else 
+							{
+								$yesChecked="";
+								$noChecked="checked";
+							}
+
+							//VALIDATION
+							//Making sure the required fields are not empty
+							if ($boothNumber== "")
+							{
+								$msg = $msg . '<br/><b>Please enter the required fields.</b>';
+							}
+							else
+							{
+								$everythingOk= true;
+							}
+					
+							//if everything is correct
+							if ($everythingOk) 
+							{
+								//direct to another page to process using query strings
+								$_SESSION['boothNumber']= $boothNumber;
+								$_SESSION['active']=$active;
+								//header("Location: process.php");
+							}                
+						}	
+					?>
+
+					<!-- Form -->
+					<form method="post" action="booth.php">
+						<div class="row uniform">
 							<?php
-								//always initialize variables to be used
-								$boothNumber = "";
-								$active = "";
-								$msg = "";
-
-								$yesChecked = "";
-								$noChecked = "";
-								$everythingOk= false;
-
-								if (isset($_POST['submit'])) //check if this page is requested after Submit button is clicked
-								{
-							
-									//take the information submitted and send to a process file
-									//always trim the user input to get rid of the additiona white spaces on both ends of the user input
-									$boothNumber = trim($_POST['boothNumber']);
-
-									//Active
-									if (isset($_POST['active']))
-										$active = trim($_POST['active']);
-									//taking the selected value for active
-									if ($active=="Yes") 
-									{
-										$yesChecked="checked";
-										$noChecked="";
-									}
-									else 
-									{
-										$yesChecked="";
-										$noChecked="checked";
-									}
-
-									//VALIDATION
-									//Making sure the required fields are not empty
-									if ($boothNumber== "")
-									{
-										$msg = $msg . '<br/><b>Please enter the required fields.</b>';
-									}
-									else
-									{
-										$everythingOk= true;
-									}
-							
-									//if everything is correct
-									if ($everythingOk) 
-									{
-										//direct to another page to process using query strings
-										$_SESSION['boothNumber']= $boothNumber;
-										$_SESSION['active']=$active;
-										//header("Location: process.php");
-									}                
-								}	
+								print $msg;
 							?>
-
-							<!-- Form -->
-							<form method="post" action="booth.php">
-								<div class="row uniform">
-									<?php
-										print $msg;
-									?>
-									<div class="12u$">
-										<b>Booth Number<sup>*</sup></b>
-										<input type="text" maxlength="3" name="boothNumber" id="boothNumber" placeholder="1" />
-									</div>
-									<!-- Break -->
-									<div class="row uniform">
-										<b>Active</b>
-										<div class="4u 12u$(small)">
-											<input type="radio" name="active" id = "yes" value = "Yes" <?php print $yesChecked; ?> checked />
-											<label for="yes">Yes</label>
-										</div>
-										<div class="4u$ 12u$(small)">
-											<input type="radio" name="active" id = "no" value = "No" <?php print $noChecked; ?> />
-											<label for="no">No</label>
-										</div>
-									</div>
-									<!-- Break -->
-									<!--Submit buttons-->
-									<div class="12u$">
-										<ul class="actions">
-											<li><input type="submit" name = "submit"  value="Add"/></li>
-											<li><input type="reset" name ="reset" value="Reset" class="alt" /></li>
-										</ul>
-									</div>
+							<div class="12u$">
+								<b>Booth Number<sup>*</sup></b>
+								<input type="text" maxlength="3" name="boothNumber" id="boothNumber" placeholder="1" />
+							</div>
+							<!-- Break -->
+							<div class="row uniform">
+								<b>Active</b>
+								<div class="4u 12u$(small)">
+									<input type="radio" name="active" id = "yes" value = "Yes" <?php print $yesChecked; ?> checked />
+									<label for="yes">Yes</label>
 								</div>
-							</form><!--close form-->
-						</div><!--close inner-->
-					</div><!--close #enter collapse-->
-				</div>
-				<!-- Break -->
-				<div class="12u$">
-					<!--For Edit-->
-					<button data-toggle="collapse" data-target="#edit"><i class="fas fa-edit"> Edit</i></button>
-					<div id="edit" class="collapse">
-						table here
-					</div><!--close #delete collapse-->
-				</div>
-				<!-- Break -->
-				<div class="12u$">
-					<!--For Delete-->
-					<button data-toggle="collapse" data-target="#delete"><i class="fas fa-trash-alt"> Delete</i></button>
-					<div id="delete" class="collapse">
-						table here
-					</div><!--close #delete collapse-->
-				</div>
-			</div><!--close container-->
-		</section><!--close section-->
+								<div class="4u$ 12u$(small)">
+									<input type="radio" name="active" id = "no" value = "No" <?php print $noChecked; ?> />
+									<label for="no">No</label>
+								</div>
+							</div>
+							<!-- Break -->
+							<!--Submit buttons-->
+							<div class="12u$">
+								<ul class="actions">
+									<li><input type="submit" name = "submit"  value="Add"/></li>
+									<li><input type="reset" name ="reset" value="Reset" class="alt" /></li>
+								</ul>
+							</div>
+						</div>
+					</form><!--close form-->
+				</div><!--close inner-->
+			</div><!--close #enter tab-->
+		</div><!--close container-->
+
 		<!-- Footer and Scripts-->
 		<?php 
-            include "footer.php";
-            include "script.php";
+			include "footer.php";
+			include "script.php";
 		?>
 	</body>
 </html>
--- script.php
+++ script.php
@@ -12,3 +12,44 @@
 <script src="assets/js/skel.min.js"></script>
 <script src="assets/js/util.js"></script>
 <script src="assets/js/main.js"></script>
+
+<!--Script for Tab-->
+<script>
+    function openPage(pageName, elmnt, color) 
+    {
+        // Hide all elements with class="tabcontent" by default */
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        }
+    
+        // Remove the background color of all tablinks/buttons
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        }
+    
+        // Show the specific tab content
+        document.getElementById(pageName).style.display = "block";
+    
+        // Add the specific color to the button used to open the tab content
+        elmnt.style.backgroundColor = color;
+    }
+    
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+</script>
+
+<!--Script for Datatable-->
+<script type="text/javascript" language="javascript" src="../../media/js/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="../../media/js/jquery.dataTables.js"></script>
+<script type="text/javascript" language="javascript" src="../resources/syntax/shCore.js"></script>
+<script type="text/javascript" language="javascript" src="../resources/demo.js"></script>
+<script type="text/javascript" language="javascript" class="init">
+
+    $(document).ready(function() {
+        $('#example').DataTable();
+    } );
+
+</script>
