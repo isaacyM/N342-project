2c11de6bc1af38dd565647dccd7295a3b4777d42
riyshres <riyshres@iu.edu>
2020-10-15 20:33:29 -0400

    checking if pulling data works

--- existAdmin.php
+++ existAdmin.php
@@ -27,9 +27,21 @@
 		<div class="container">	
 			
             <h1>Existing Content</h1>
-            <p>gggg</p>
-            <input type="reset" name ="reset" value="Reset" class="alt" />
-            <h2>ggg</h2>
+			<?php
+				$query = "SELECT * FROM ADMIN"; //You don't need a ; like you do in SQL
+				$result = mysql_query($query);
+
+				echo "<table>"; // start a table tag in the HTML
+
+				while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
+				echo "<tr><td>" . $row['AdminID'] . "</td></tr>" .$row['FirstName'] . "</td><td>" . $row['LastName'] . "</td></tr>" . $row['MiddleName'] . "</td></tr>" 
+				. $row['Email'] . "</td></tr>". $row['Password'] . "</td></tr>". $row['Level'] . "</td></tr>" . $row['Active'] . "</td></tr>";  //$row['index'] the index here is a field name
+				}
+
+				echo "</table>"; //Close the table in HTML
+
+				mysql_close(); //Make sure to close out the database connection
+			?>
         </div>
 
 		<!-- Footer and Scripts-->
