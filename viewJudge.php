a680afc9a1afd56bda344e4259940df1cfe593ee
riyshres <riyshres@iu.edu>
2020-11-05 21:58:14 -0500

    s

--- viewJudge.php
+++ viewJudge.php
@@ -33,7 +33,7 @@
                 print '<thead><tr><th>JudgeID</th><th>First Name</th><th>Middle Name</th><th>Last Name</th><th>Title</th><th>Degree Earned</th><th>Employer</th>
                 <th>Email</th><th>Username</th><th>Check In</th></tr></thead>
 				<tfoot><tr><th>JudgeID</th><th>First Name</th><th>Middle Name</th><th>Last Name</th><th>Title</th><th>Degree Earned</th><th>Employer</th>
-                <th>Email</th><th>Username</th><th>Password</th><th>Check In</th></tr></tfoot>';
+                <th>Email</th><th>Username</th><th>Check In</th></tr></tfoot>';
 
 				while($row = mysql_fetch_array($result))
 				{   
@@ -41,7 +41,7 @@
 					print "<tr>";
 					print "<td>".$row["JudgeID"]."</td><td>".$row["FirstName"]."</td><td>".$row["MiddleName"]."</td><td>".$row["LastName"]."</td><td>"
 					.$row["Title"]."</td><td>".$row["HighestDegreeEarned"]."</td><td>".$row["Employer"]."</td><td>".$row["Email"]."</td><td>"
-					.$row["Username"]."</td><td>".$row["Password"]."</td><td>".$row["CheckIn"]."</td>";
+					.$row["Username"]."</td><td>".$row["CheckIn"]."</td>";
 					print "</tr>";
 				}
 				print "</table>"; //Close the table in HTML
